7db90c3f6950f09ad817ea51379cbfdd302652f8
TristanLemire <tristan.lemire.sio@gmail.com>
2019-07-03 16:53:15 +0200

    finish site data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,26 @@ import React from "react"
 import Link from 'gatsby-link'
 import Counter from './counter'
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default () => <Layout style={{color: 'tomato'}}>
-                      <h1>Hello World</h1>
-                      <p>This is a paragraph</p>
+export default ({ data }) => ( <Layout style={{color: 'tomato'}}>
+                      <h1>{data.site.siteMetadata.title}</h1>
+                      <p>Author: {data.site.siteMetadata.author}</p>
                       <Link to="/page-2/">Page 2</Link>
                       <br/>
                       <Link to="/dir1/page-3/">Page 3</Link>
                       <br/>
                       <Counter color="green" />
                     </Layout>
+)
+
+export const query = graphql`
+  query FirstQuery {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }
+`
